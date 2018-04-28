399a7cb6e974b64153564783693c876090b2d55b
Marknetreba <mark.netreba@mail.ru>
2018-04-29 00:10:09 +0400

    fcm hard commit

--- renderer.js
+++ renderer.js
@@ -4,6 +4,25 @@
 
 const { ipcRenderer } = require ('electron');
 const axios = require('axios');
+const admin = require("firebase-admin");
+const firebase = require("firebase");
+const topics = require('topics');
+
+const config = {
+    apiKey: "AIzaSyA6dkZsqREfubEGyxN4yAEyf2P6np_RP6Y",
+    authDomain: "infoterminal-7973d.firebaseapp.com",
+    databaseURL: "https://infoterminal-7973d.firebaseio.com",
+    projectId: "infoterminal-7973d",
+    storageBucket: "",
+    messagingSenderId: "498398049541"
+};
+firebase.initializeApp(config);
+
+
+admin.initializeApp({
+    credential: admin.credential.cert("infoterminal-admin-key.json"),
+    databaseURL: "https://infoterminal-7973d.firebaseio.com"
+});
 
 const {
   START_NOTIFICATION_SERVICE,
@@ -16,15 +35,23 @@ const {
 // Listen for service successfully started
 ipcRenderer.on(NOTIFICATION_SERVICE_STARTED, (_, token) => {
   console.log('Service started -> ', token);
-  
-    // axios.post('https://iid.googleapis.com/iid/v1/REGISTRATION/rel/topics/55_55',{}, {
-    //   headers: {
-    //       "Content-Type" : "application/json",
-    //       "Authorization" : "key="+token
-    //   }
-    // }).then(function (response) {
-    //     console.log(response)
-    // })
+
+    // admin.messaging().subscribeToTopic(token, "news").then(function (response) {
+    //         console.log("Successfully subscribed to topic:", response);
+    //     }).catch(function (error) {
+    //         console.log("Error subscribing to topic:", error);
+    //     });
+
+    const auth = "AAAAdArWtQU:APA91bEBGdgYLIUuX0_9H7MITtswX8Eu4YYMfNDUoVMfInHz0ueCtIL1JBtPRRbzievC3JhLApscOsx7zhpSNkxkJ5He8QjnXJFB5MQ6tQuhjv2zW6jUqhmBLuT7QYs0brG_73vJt5iT";
+
+    axios.post('https://iid.googleapis.com/iid/v1/'+token+'/rel/topics/55',{}, {
+      headers: {
+          "Content-Type" : "application/json",
+          "Authorization" : "key="+auth
+      }
+    }).then(function (response) {
+        console.log(response)
+    })
 });
 
 // Handle notification errors
@@ -60,6 +87,3 @@ ipcRenderer.on(NOTIFICATION_RECEIVED, (_, serverNotificationPayload) => {
 const senderId = '498398049541'; // <-- replace with FCM sender ID from FCM web admin under Settings->Cloud Messaging
 console.log('starting service and registering a client');
 ipcRenderer.send(START_NOTIFICATION_SERVICE, senderId);
-
-
-
